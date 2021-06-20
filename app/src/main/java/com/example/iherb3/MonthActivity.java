fd1b6644d31edcafa15f2761475b23a564861d86
Nesquika <63696662+Nesquika@users.noreply.github.com>
2021-06-20 09:35:09 +0400

    month calendar added

--- app/src/main/java/com/example/iherb3/MonthActivity.java
+++ app/src/main/java/com/example/iherb3/MonthActivity.java
@@ -1,49 +1,43 @@
 package com.example.iherb3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class DayActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MonthActivity extends AppCompatActivity {
     ImageView menu;
-    int[] progress = new int[30];
-    CheckBox chb;
-    ProgressBar progressBar;
-    Intent month;
+    Intent day;
+    TextView current;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_day);
-        month = new Intent(this, MonthActivity.class);
+        setContentView(R.layout.activity_month);
         menu = findViewById(R.id.menu);
         menu.setOnClickListener(viewClickListener);
-        chb = findViewById(R.id.checkbox);
-        progressBar = findViewById(R.id.progress);
-        chb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    progressBar.setProgress(100);
-                    month.putExtra("progress", 100);
-                } else {
-                    progressBar.setProgress(50);
-                    month.putExtra("progress", 50);
-                }
-            }
-        });
+        current = findViewById(R.id.current);
+        day = getIntent();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Bundle extras = getIntent().getExtras();
+        int pr;
+        if (extras != null) pr = extras.getInt("progress");
+        if (day.getIntExtra("progress", 50) == 100) current.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
+        else current.setBackgroundColor(ContextCompat.getColor(this, R.color.orange));
+    }
 
     View.OnClickListener viewClickListener = new View.OnClickListener() {
         @Override
@@ -96,8 +90,9 @@ public class DayActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void openMonth(View view){
-        startActivity(month);
+    public void openDay(View view) {
+        Intent intent = new Intent(this, DayActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
     }
-
 }
